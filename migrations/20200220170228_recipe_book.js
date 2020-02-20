9f38993ab1eb81ab4db9453f3f1b2526e5ab909d
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-20 17:49:50 -0500

    created migration

---
+++ migrations/20200220170228_recipe_book.js
@@ -0,0 +1,63 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('recipes', tbl => {
+      tbl.increments()
+
+      tbl.string('name', 255)
+      .notNullable()
+      .unique();
+  })
+  .createTable('ingredients', tbl => {
+      tbl.increments()
+
+      tbl.string('name', 255)
+      .notNullable()
+
+      tbl.float('quantity')
+      .notNullable();
+  })
+  .createTable('recipe_ingredients', tbl => {
+      tbl.primary(['recipe_id', 'ingredient_id'])
+
+      tbl.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+
+      tbl.integer('ingredient_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('ingredients');
+  })
+  .createTable('steps', tbl => {
+      tbl.increments()
+
+      tbl.string('instruction')
+      .notNullable();
+  })
+  .createTable('recipe_steps', tbl => {
+    tbl.primary(['recipe_id', 'step_id'])
+
+    tbl.integer('recipe_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('recipes')
+
+    tbl.integer('step_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('steps');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('recipe_steps')
+  .dropTableIfExists('steps')
+  .dropTableIfExists('recipe_ingredients')
+  .dropTableIfExists('ingredients')
+  .dropTableIfExists('recipes')
+};
